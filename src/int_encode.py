74893876077048100a75eb16e5e8c28d565ecc7b
Anatoli Stetchi <acetic.chortle24@icloud.com>
2026-03-29 22:06:43 +0300

    simplify int_encode function by removing manual digit processing

--- src/int_encode.py
+++ src/int_encode.py
@@ -2,17 +2,4 @@ def int_encode(bytes: bytes) -> int:
     if bytes.startswith(b"-0"):
         raise ValueError("Starts with -0")
 
-    negative = bytes.startswith(b"-")
-    num = 0
-
-    if negative:
-        bytes = bytes[1:]
-
-    for place, digit in enumerate(bytes[::-1]):
-        digit -= 48
-        if 0 < digit > 9:
-            raise TypeError(f"{digit} is not a digit")
-
-        num += digit * (10**place)
-
-    return num if not negative else num * -1
+    return int(bytes)
