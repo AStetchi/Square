39dcddd6e64f7ceb5b229e2d58ad3e5339b0b123
Anatoli Stetchi <acetic.chortle24@icloud.com>
2026-03-30 20:23:53 +0300

    add docstring to int_encode

--- src/int_encode.py
+++ src/int_encode.py
@@ -1,5 +1,11 @@
-def int_encode(bytes: bytes) -> int:
-    if bytes.startswith(b"-0"):
+def int_encode(data: bytes) -> int:
+    """
+    This module provides a function to encode bytes into an integer.
+    It checks for a specific case where the byte string starts with "-0"
+    and raises a ValueError if that is the case. Otherwise, it converts
+    the byte string to an integer using the built-in int() function.
+    """
+    if data.startswith(b"-0"):
         raise ValueError("Starts with -0")
 
-    return int(bytes)
+    return int(data)
